bd066ff4ad128c61a16843e92d16e802ec7ae948
Tommy Klumker <tommy@smithandrobot.com>
2011-01-14 15:14:32 -0600

    added toSting to interaction even that reflects the interaction name associated with it

--- test/integration/mudu/api/AchievementRestControllerTests.groovy
+++ test/integration/mudu/api/AchievementRestControllerTests.groovy
@@ -1,9 +1,20 @@
 package mudu.api
 import grails.converters.JSON
+import mudu.Achievement
+import mudu.EarnedAchievement
+import mudu.Player
 
 class AchievementRestControllerTests extends GroovyTestCase {
+
   def achievementService
+  def interactionService
+  def fid = "00000"
+  def irc = new InteractionRestController()
+
   protected void setUp() {
+    irc.interactionService = interactionService
+    irc.params.facebookId = fid
+    irc.params.interactionId = "1"
     super.setUp()
   }
 
@@ -17,6 +28,16 @@ class AchievementRestControllerTests extends GroovyTestCase {
     arc.index()
     def resp = JSON.parse(arc.response.contentAsString)
     assertEquals resp[0].name, "Invite"
+  }
+
+  void testInviteAchievementAwarded() {
+    irc.params.interactionId = "1"
+    irc.create()
+    def resp = JSON.parse(irc.response.contentAsString)
+    assertEquals resp.stat, "ok"
+    def a = EarnedAchievement.findByPlayer(Player.findByFacebookId(fid))
+    assertNotSame null, a
+    assertEquals "Invite", a.achievement.name
   }
 
 
